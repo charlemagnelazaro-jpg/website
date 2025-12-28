8062c18c54f0845f7457df26510dd4bab3925547
charlemagnelazaro-jpg <charlemagne_lazaro@dlsu.edu.ph>
2025-12-28 21:58:20 +0800

    Adding JS
    Tried to study how the button works and how the JS is connected to the HTML

---
+++ script.js
@@ -0,0 +1,6 @@
+let Letterbtn = document.querySelector('.Letterbtn');
+let letterContent2 = "I miss you so much love, I can't wait to get to hold you. I hope we can get to spend the holidays together in the future. I love you mostest my pretty girl."
+
+Letterbtn.addEventListener('click', () => {
+    document.querySelector("#words").textContent = letterContent2;
+});
