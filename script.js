d4da0b620eb6669c6c673e750a2d76745aa0dd20
charlemagnelazaro-jpg <charlemagne_lazaro@dlsu.edu.ph>
2025-12-30 01:17:30 +0800

    Update on letters
    Made letters an array and added transition

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-let Letterbtn = document.querySelector('.Letterbtn');
-let letterContent2 = "I miss you so much love, I can't wait to get to hold you. I hope we can get to spend the holidays together in the future. I love you mostest my pretty girl."
+let words = document.querySelector('#words');
 
-Letterbtn.addEventListener('click', () => {
-    document.querySelector("#words").textContent = letterContent2;
+let currentLetterIndex = 0;
+
+let secondLetter = "I miss you so much love, I can't wait to get to hold you. I hope we can get to spend the holidays together in the future. I love you mostest my pretty girl."
+let firstLetter = "Hi, my love! I know that I don't get to give you letters as much as bouquets so I made this website for you. You are the greatest person I have ever met and I am so lucky to have you in my life. No words can truly express how lucky I am to have you as my girlfriend. I hope that my actions can speak louder than any words I say. I promise that through every hardship and difficulty, I will always be by your side, supporting and loving you. Just like this website, I will learn how to be better so it can flourish but at the same time we can look back and appreiciate how far we have come. I love you mostest my pretty girl."
+let thirdLetter = "2 more days before I can get to say that I'm ending the year with you, and starting the next one with the prettiest girl I've ever met. I love you so much my love and it grows each day regardless of the distance. You will always be the main reason why I wan't to be better, you make me better. I love you mostest my pretty wife."
+
+let letterCollection  = [firstLetter, secondLetter, thirdLetter];
+
+words.textContent = letterCollection[currentLetterIndex];
+
+changeLetter = (direction) => {
+
+    words.style.opacity = 0;
+    words.offsetHeight;
+
+    
+    setTimeout(() => {
+        if (direction === 'next') {
+            currentLetterIndex = (currentLetterIndex + 1) % letterCollection.length;
+        } else if (direction === 'prev') {
+            currentLetterIndex = (currentLetterIndex - 1 + letterCollection.length) % letterCollection.length;
+        }
+        words.textContent = letterCollection[currentLetterIndex];
+        words.style.opacity = 1;
+    }, 500);
+}
+document.querySelector('.NextLetterButton').addEventListener('click', () => {
+    changeLetter('next');
+});
+document.querySelector('.PrevLetterButton').addEventListener('click', () => {
+    changeLetter('prev');
 });
